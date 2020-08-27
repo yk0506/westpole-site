3e1b609cffb299cceeec06e2be6ca6d35fe877b2
puchi <yunkyung0506@gmail.com>
2020-08-27 10:56:17 +0900

    init

---
+++ src/main/java/biz/westpole/WestpoleSiteApplication.java
@@ -0,0 +1,13 @@
+package biz.westpole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestpoleSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestpoleSiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/biz/westpole/controller/MainController.java
@@ -0,0 +1,13 @@
+package biz.westpole.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main() {
+		return "westpole/contents/index";
+	}
+}
