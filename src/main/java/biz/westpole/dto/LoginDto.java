cf6985a5c6ef8cbc28e60ee7dc76649a6a57da47
puchi <yunkyung0506@gmail.com>
2020-08-31 10:48:50 +0900

    로그인처

---
+++ src/main/java/biz/westpole/dto/LoginDto.java
@@ -0,0 +1,29 @@
+package biz.westpole.dto;
+
+public class LoginDto {
+
+	private String id;
+	private String password;
+	private String message;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
